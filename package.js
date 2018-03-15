9d68122f5afaf0e24b0914a3a71d4d7e45ed707e
Theodor Diaconu <diaconu.theodor@gmail.com>
2018-03-15 17:48:34 +0200

    setup for atmosphere

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
   name: 'cultofcoders:collection-names',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: "Find collection by it's name",
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cult-of-coders/meteor-collection-names',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1');
+  api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('mongo');
   api.mainModule('collection-names.js');
