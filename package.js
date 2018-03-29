e207e9fa36c2b10ed78cfb0d1c4a7fc1b7e717ff
Theodor Diaconu <diaconu.theodor@gmail.com>
2018-03-29 16:09:25 +0300

    removed underscore dependency

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'cultofcoders:collection-names',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: "Find collection by it's name",
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/cult-of-coders/meteor-collection-names',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'cultofcoders:collection-names',
+    version: '0.0.2',
+    // Brief, one-line summary of the package.
+    summary: "Find collection by it's name",
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/cult-of-coders/meteor-collection-names',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md',
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
-  api.use('ecmascript');
-  api.use('mongo');
-  api.mainModule('collection-names.js');
+    api.versionsFrom('1.3');
+    api.use('ecmascript');
+    api.use('mongo');
+    api.mainModule('collection-names.js');
 });
